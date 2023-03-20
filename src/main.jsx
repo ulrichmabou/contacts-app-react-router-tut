3ae3aeb17f8dc5e1c42eb19fd946c012a11747bb
Ulrich Mabou <ulrichmabou@MacBook-Pro-2.local>
2023-03-20 12:17:12 +0200

    add error handling, nexted routes and client side rendering

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root from './routes/root';
+import Root, { loader as rootLoader } from './routes/root';
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-  }
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "/contacts/:contactId",
+        element: <Contact />
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
