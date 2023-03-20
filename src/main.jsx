0a951e29fb923b8a7891496a930727554bc524b1
Ulrich Mabou <ulrichmabou@MacBook-Pro-2.local>
2023-03-20 14:17:24 +0200

    added loader to contact

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Root, { loader as rootLoader } from './routes/root';
+import Root, { 
+  loader as rootLoader,
+  action as rootAction,
+ } from './routes/root';
 import ErrorPage from './error-page';
-import Contact from './routes/contact';
+import Contact, { loader as contactLoader} from './routes/contact';
 import './index.css';
 
 const router = createBrowserRouter([
@@ -12,10 +15,12 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "/contacts/:contactId",
-        element: <Contact />
+        element: <Contact />,
+        loader: contactLoader,
       },
     ],
   },
