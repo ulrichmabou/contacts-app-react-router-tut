1d5b023e1175d82356ebedce5f7616472f5fb467
Ulrich Mabou <ulrichmabou@MacBook-Pro-2.local>
2023-03-20 15:02:44 +0200

    added active link styling

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,8 +1,9 @@
-import { 
-    Outlet, 
-    Link, 
-    useLoaderData, 
-    Form 
+import {
+    Outlet,
+    NavLink,
+    useLoaderData,
+    Form,
+    redirect,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
@@ -13,7 +14,7 @@ export async function loader() {
 
 export async function action() {
     const contact = await createContact();
-    return { contact };
+    return redirect(`/contacts/${contact.id}/edit`);
 }
 
 export default function Root() {
@@ -51,7 +52,16 @@ export default function Root() {
                         <ul>
                             {contacts.map((contact) => (
                                 <li key={contact.id}>
-                                    <Link to={`contacts/${contact.id}`}>
+                                    <NavLink
+                                        to={`contacts/${contact.id}`}
+                                        className={({ isActive, isPending }) =>
+                                            isActive
+                                                ? "active"
+                                                : isPending
+                                                    ? "pending"
+                                                    : ""
+                                        }
+                                    >
                                         {contact.first || contact.last ? (
                                             <>
                                                 {contact.first} {contact.last}
@@ -60,7 +70,7 @@ export default function Root() {
                                             <i>No Name</i>
                                         )}{" "}
                                         {contact.favorite && <span>★</span>}
-                                    </Link>
+                                    </NavLink>
                                 </li>
                             ))}
                         </ul>
