4e80229d6616ae1dca16cdef2b69c8f320ed5646
Ulrich Mabou <ulrichmabou@gmail.com>
2023-03-21 12:57:36 +0200

    added search spinner

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import {
     Outlet,
     NavLink,
@@ -5,14 +6,15 @@ import {
     redirect,
     useLoaderData,
     useNavigation,
+    useSubmit,
 } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
 
 export async function loader({ request }) {
     const url = new URL(request.url);
     const q = url.searchParams.get("q");
-    const contacts = await getContacts(q); 
-    return { contacts };
+    const contacts = await getContacts(q);
+    return { contacts, q };
 }
 
 export async function action() {
@@ -21,8 +23,19 @@ export async function action() {
 }
 
 export default function Root() {
-    const { contacts } = useLoaderData();
+    const { contacts, q } = useLoaderData();
     const navigation = useNavigation();
+    const submit = useSubmit();
+
+    const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+      "q"
+    );
+
+    useEffect(() => {
+        document.getElementById("q").value = q;
+    }, [q]);
 
     return (
         <>
@@ -32,15 +45,20 @@ export default function Root() {
                     <Form id="search-form" role="search">
                         <input
                             id="q"
+                            className={searching ? "loading" : ""}
                             aria-label="Search contacts"
                             placeholder="Search"
                             type="search"
                             name="q"
+                            defaultValue={q}
+                            onChange={(event) => {
+                                submit(event.currentTarget.form);
+                            }}
                         />
                         <div
                             id="search-spinner"
                             aria-hidden
-                            hidden={true}
+                            hidden={!searching}
                         />
                         <div
                             className="sr-only"
